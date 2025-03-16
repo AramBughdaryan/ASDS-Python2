8204c293b03060261c33d5c3088d970a9d8c0487
Aram <arambughdaryan@gmail.com>
2025-03-16 20:27:14 +0400

    [Update & Add] rearrange files and add some part of lecture 5

---
+++ lecture/Lecture05/3.iterable.py
@@ -0,0 +1,18 @@
+from collections.abc import Iterable
+
+# Iterable only needs __iter__ function to be implemented
+
+def square_elements(it: Iterable[int]) -> list[int]:
+    return [x ** 2 for x in it]
+
+
+def gen():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
+
+square_elements([1, 2, 3, 4, 5])
+square_elements(range(1, 6))
+square_elements(gen())
